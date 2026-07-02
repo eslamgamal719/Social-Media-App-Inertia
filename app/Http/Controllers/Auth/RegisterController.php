69d9bc4019e94a71f5713b0d96b654e563a68132
eslamgamal719 <eslamgamal719a@gmail.com>
2026-07-02 15:44:20 +0300

    add_login_and_registeration_forms

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RegisterController extends Controller
+{
+    public function create(): Response
+    {
+        return Inertia::render('auth/register/create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostToggleLike;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return Inertia::render('home');
@@ -15,6 +17,13 @@ Route::get('/about', function () {
 })->name('about.index');
 
 Route::post('/posts/{post}/likes/toggle', PostToggleLike::class);
+
+Route::get('/auth/register', [RegisterController::class, 'create']);
+Route::post('/auth/register', [RegisterController::class, 'store']);
+
+Route::get('/auth/login', [LoginController::class, 'create']);
+Route::post('/auth/login', [LoginController::class, 'store']);
+Route::post('/auth/logout', [LoginController::class, 'destroy']);
 
 //Posts
 Route::get('/posts/create', [PostController::class, 'create']);
